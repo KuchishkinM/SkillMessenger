08f8fe71587ad3713c764a30826812758ced524f
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-11-03 15:52:12 +0300

    update project

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
-all_messages = []
+DATA_FILE = 'data.json'
+
+
+def load_messages():
+    with open(DATA_FILE, 'r') as json_file:
+        data = json.load(json_file)
+        return data['all_messages']
+
+
+all_messages = load_messages()
+
+
+def save_messages():
+    with open(DATA_FILE, 'w') as json_file:
+        data = {'all_messages': all_messages}
+        json.dump(data, json_file)
 
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello, World!</p>'
+    return "<p>Hello, welcome to <b>Skill</b> <i>Messenger</i>!</p>" \
+           "<p><a href=\"/chat\">Chat</a></p>" \
+           "<p><a href=\"/info\">Info</a></p>"
 
 
 @app.route('/get_messages')
@@ -23,12 +41,16 @@ def add_message(sender, text):
         "text": text,
         "time": current_time,
     }
+    all_messages.append(new_message)
+    save_messages()
     # Ограничение: не более 100 сообщений в чате
-    if len(all_messages) > 99:
-        all_messages.pop(0)
-        all_messages.append(new_message)
-    else:
-        all_messages.append(new_message)
+    # if len(all_messages) > 99:
+    #    all_messages.pop(0)
+    #    all_messages.append(new_message)
+    #    save_messages()
+    # else:
+    #    all_messages.append(new_message)
+    #    save_messages()
 
 
 @app.route('/send_message')
@@ -56,10 +78,17 @@ def send_message():
     return {'result': True}
 
 
+@app.route("/info")
+def info_page():
+    return f"<p>Total of messages in chat: <b>{len(all_messages)}</b></p>" \
+           f"<p><a href=\"/../chat\">Chat</a></p>" \
+           f"<p><a href=\"/..\">Main page</a></p>"
+
+
 @app.route('/chat')
 def chat_page():
     return render_template('form.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
